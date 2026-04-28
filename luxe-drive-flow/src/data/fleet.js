9a85e9b8e419a9d78a4f652526f0c787c3e54787
robertacomella-png <robert.a.comella@gmail.com>
2026-04-28 11:21:18 -0400

    Update fleet.js

--- luxe-drive-flow/src/data/fleet.js
+++ luxe-drive-flow/src/data/fleet.js
@@ -1,1 +1,22 @@
-
+export const fleet = [
+  {
+    id: 1,
+    name: "Lamborghini Urus",
+    make: "Lamborghini",
+    year: 2023,
+    price: 999,
+    power: 641,
+    miles: 100,
+    image: "/gallery/gallery-2.jpg",
+  },
+  {
+    id: 2,
+    name: "GLS 600 Maybach",
+    make: "Mercedes-Benz",
+    year: 2023,
+    price: 899,
+    power: 550,
+    miles: 100,
+    image: "/gallery/gallery-1.jpg",
+  },
+];
