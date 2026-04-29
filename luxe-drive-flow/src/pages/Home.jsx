73d4dfc75b72f6e7b247ef68c15a758078fcbe94
robertacomella-png <robert.a.comella@gmail.com>
2026-04-28 23:55:36 -0400

    Update Home.jsx

--- luxe-drive-flow/src/pages/Home.jsx
+++ luxe-drive-flow/src/pages/Home.jsx
@@ -5,26 +5,30 @@ import ExperienceSection from "../components/experience/ExperienceSection";
 import ProcessSection from "../components/process/ProcessSection";
 import TestimonialsSection from "../components/testimonials/TestimonialsSection";
 import CTASection from "../components/cta/CTASection";
-import Footer from "../components/footer/Footer";
 
 export default function Home() {
   return (
     <div className="bg-black">
       <Navbar />
       <HeroSection />
+
       <div id="experience">
         <FleetSection />
       </div>
+
       <ExperienceSection />
+
       <div id="process">
         <ProcessSection />
       </div>
+
       <div id="testimonials">
         <TestimonialsSection />
       </div>
+
       <div id="cta">
         <CTASection />
-      
+      </div>
     </div>
   );
 }
